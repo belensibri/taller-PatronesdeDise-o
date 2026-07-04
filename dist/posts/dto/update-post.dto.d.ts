c54dea42bf0269b5438c82d0f76ba5296fb5f00f
Dae51 <josue.portillo0123@gmail.com>
2026-07-04 11:25:35 -0600

    limpieza

--- dist/posts/dto/update-post.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { PostStatus } from '../entities/post.entity';
-export declare class UpdatePostDto {
-    title?: string;
-    content?: string;
-    excerpt?: string | null;
-    slug?: string;
-    status?: PostStatus;
-    author_id?: number;
-}
